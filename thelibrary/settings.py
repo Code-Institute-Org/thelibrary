6c03a68c46bca5492c8a2fc4126b847ff9c037b2
Anna Greaves <gilhespy.anna@gmail.com>
2021-03-22 22:20:14 +0100

    Add new secret key in environment variables.

--- thelibrary/settings.py
+++ thelibrary/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nottherealkey'
+SECRET_KEY = os.environ.get('SECRET') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
