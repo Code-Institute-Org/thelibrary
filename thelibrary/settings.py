1c1dba52648abbbbe9abd9ace426ea4cfee7a094
Anna Greaves <gilhespy.anna@gmail.com>
2021-03-22 22:10:59 +0100

    Remove old secret key.

--- thelibrary/settings.py
+++ thelibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')el$z0iz!b78e^@#op-km5t$)gj)ogne4ow%1ktp1-0oz5f2ko'
+SECRET_KEY = 'nottherealkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
