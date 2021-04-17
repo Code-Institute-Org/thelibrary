5201bbc4611f6d75e1b8d151c57e9b167e838b4f
Anna Greaves <gilhespy.anna@gmail.com>
2021-04-17 14:13:47 +0200

    Update Post model to return status in string.

--- posts/models.py
+++ posts/models.py
@@ -8,13 +8,21 @@ class Post(models.Model):
     """
     Create instance of Post
     """
-    title = models.CharField(max_length=300)
+    WAITING = 'Waiting'
+    APPROVED = 'Approved'
+    DEACTIVATED = 'Deactivated'
+    STATUS_CHOICES = (
+        (WAITING, "Awaiting Approval"),
+        (APPROVED, "Approved"),
+        (DEACTIVATED, "Deactivated")
+    )
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateField(default=datetime.date.today)
-    date_last_updated = models.DateField(null=True, blank=True)
-    is_approved = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    created_on = models.DateField(default=datetime.date.today)
+    updated_on = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=WAITING)
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.title} | {self.status}"
