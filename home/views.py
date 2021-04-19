da26c7ede60efd1251ca5a0eac9f61885894f36f
Anna Greaves <gilhespy.anna@gmail.com>
2021-04-19 22:28:22 +0200

    Add recent, popular and posts by category to home page.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
+from posts.models import Post, PostCategory
+
 
 # Create your views here.
 def home_view(request):
     """
     Render home page
     """
-    return render(request, 'home/index.html')
+    recent_posts = Post.objects.filter(
+        status="Approved").order_by('-created_on')[:5]
+    favourite_posts = Post.objects.filter(
+        status="Approved"
+    ).annotate(like_count=Count('likes')).order_by('-like_count')[:5]
+    
+    categories = PostCategory.objects.all()
+    items_by_category = []
+    for cat in categories:
+        items_by_category += Post.objects.filter(status="Approved", category=cat).order_by('?')[:1]
+    
+    context = {
+        'recent_posts': recent_posts,
+        'favourite_posts': favourite_posts,
+        'items_by_category': items_by_category
+
+    }
+    return render(request, 'home/index.html', context)
