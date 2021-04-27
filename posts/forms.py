56dd65c688af9b51f4718406d8932ff222bb7e3d
Anna Greaves <gilhespy.anna@gmail.com>
2021-04-27 11:37:07 +0200

    Add and sty;e flag modal to post detail.

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from .models import PostFlag
 
 class FlagForm(forms.ModelForm):
+    INNAPPROPRIATE = 'Innappropriate content'
+    OUTDATED = 'Outdated content'
+    FLAG_REASONS = (
+        (INNAPPROPRIATE, "Innappropriate content"),
+        (OUTDATED, "Outdated content"),
+    )
+    reason = forms.ChoiceField(
+        label="Please select the reason for flagging this post",
+        choices=FLAG_REASONS,
+        widget=forms.Select(attrs={'class': "form-select"}),
+    )
+    
     class Meta:
         model = PostFlag
         fields = ['reason']
