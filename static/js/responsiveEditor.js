c8fc1dac292a089d6627437e6233e3d1259b3474
Anna Greaves <gilhespy.anna@gmail.com>
2021-05-04 14:22:41 +0200

    Add image file upload and rich text editor design to edit page.

---
+++ static/js/responsiveEditor.js
@@ -0,0 +1,7 @@
+/**
+ * Adjusts width of installed ckeditor rich text editor
+ * after it has loaded in the DOM, to make it responsive.
+ */
+setTimeout(function(){
+    document.getElementById('cke_id_body').style.width = '100%';
+}, 500);
