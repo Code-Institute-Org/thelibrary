81059f6b45fa661592ba2366b7c106675e5b823a
Anna Greaves <gilhespy.anna@gmail.com>
2021-05-24 14:48:25 +0200

    Add tests for Post model.

--- posts/models.py
+++ posts/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
-import datetime
-
 from users.models import UserProfile
 
 
--- posts/test_models.py
+++ posts/test_models.py
@@ -1,3 +1,5 @@
+import datetime
+from users.models import UserProfile
 from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import PostTag, PostCategory, PostFlag, Post
@@ -12,7 +14,7 @@ class PostTagTestCase(TestCase):
     def test_tag_created(self):
         tag_count = PostTag.objects.all().count()
         self.assertEqual(tag_count, 1)
-    
+
     def test_tag_has_correct_name(self):
         tag = PostTag.objects.get(pk=1)
         self.assertEqual(tag.name, 'testTag')
@@ -65,9 +67,97 @@ class PostFlagTestCase(TestCase):
             str(flag), 'flag by testUsername | Innappropriate content')
 
 
-# class PostTestCase(TestCase):
-#     def setUp(self):
-#         post = Post(title='Test post title')
-#         post.summary = 'Test post summary'
-#         post.slug = 'test-post-summary'
-#         post.author
+class PostTestCase(TestCase):
+    def setUp(self):
+        user = User(username="testUsername", email="test@email.com")
+        user.set_password('testing321')
+        user.save()
+
+        category = PostCategory(name='test category')
+        category.save()
+
+        post = Post(title="post title")
+        post.summary = "post summary"
+        post.slug = "post-title"
+        post.body = 'body'
+        post.author = user.userprofile
+        post.category = category
+        post.save()
+
+    def test_post_exists(self):
+        post = Post.objects.get(pk=1)
+        user = User.objects.get(pk=1)
+        self.assertIsNotNone(post)
+        self.assertEqual(post.title, "post title")
+        self.assertEqual(post.summary, "post summary")
+        self.assertEqual(post.body, "body")
+        self.assertIsInstance(post.author, UserProfile)
+        self.assertEqual(post.author, user.userprofile)
+        self.assertIsInstance(post.category, PostCategory)
+        self.assertEqual(str(post.category), 'test category')
+
+    def test_post_default_values(self):
+        post = Post.objects.get(pk=1)
+        self.assertEqual(post.status, "Submitted")
+        self.assertIsNone(post.updated_on)
+        self.assertIsNone(post.mod_message)
+        self.assertIsNone(post.moderator)
+        self.assertIsNone(post.flag)
+        self.assertIsNone(post.editors_note)
+        self.assertEqual(str(post.image_1), '')
+        self.assertEqual(str(post.image_2), '')
+        self.assertEqual(str(post.image_3), '')
+        self.assertEqual(str(post.image_4), '')
+
+    def test_update_default_fields(self):
+        mod = User.objects.create(username="mod", email="mod@email.com")
+        mod.set_password('testing321')
+        mod.save()
+
+        flagger = User.objects.create(
+            username="flagger", email="flagger@email.com")
+        flagger.set_password('testing321')
+        flagger.save()
+
+        flag = PostFlag(flagger=flagger)
+        flag.save()
+
+        post = Post.objects.get(pk=1)
+        post.status = "Published"
+        post.mod_message = 'test mod message'
+        post.moderator = mod
+        post.flag = flag
+        post.editors_note = 'test editors note'
+        post.save()
+
+        self.assertEqual(post.status, "Published")
+        self.assertEqual(post.mod_message, 'test mod message')
+        self.assertEqual(post.moderator.username, 'mod')
+        self.assertIsInstance(post.moderator, User)
+        self.assertIsInstance(post.flag, PostFlag)
+        self.assertEqual(post.editors_note, 'test editors note')
+        
+    def test_post_likes(self):
+        user_a = User(username="user_a", email="a@email.com")
+        user_a.set_password("test321")
+        user_a.save()
+
+        user_b = User(username="user_b", email="b@email.com")
+        user_b.set_password("test321")
+        user_b.save()
+
+        post = Post.objects.get(pk=1)
+        post.likes.add(user_a)
+        post.likes.add(user_b)
+        post.save()
+
+        self.assertEqual(post.total_likes(), 2)
+
+    def test_post_dunder_string(self):
+        post = Post.objects.get(pk=1)
+        self.assertEqual(str(post), 'post title')
+
+    def test_get_absolute_url(self):
+        post = Post.objects.get(pk=1)
+        url = post.get_absolute_url()
+        self.assertEqual(url, '/posts/post/1/post-title/')
