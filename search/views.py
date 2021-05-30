addf4e3e1aa8ff2892ad8c781c2955fec0513901
Anna Greaves <gilhespy.anna@gmail.com>
2021-05-30 13:58:09 +0200

    Style search pg and pagination.

--- search/views.py
+++ search/views.py
@@ -9,7 +9,7 @@ from posts.models import Post
 class SearchResultsView(LoginRequiredMixin, ListView):
     """ Render search results with pagination """
     template_name = "search/search_results.html"
-    paginate_by = 4
+    paginate_by = 12
     context_object_name = 'posts'
 
     def get_queryset(self):
@@ -17,11 +17,26 @@ class SearchResultsView(LoginRequiredMixin, ListView):
         q = self.request.GET.get('q')
         print(q)
 
-        return Post.objects.filter(
-            Q(title__icontains=q)
-            | Q(summary__icontains=q)
-            | Q(body__icontains=q),
-            status="Published")
+        if '4P' not in q and '5P' not in q:
+            queryset = Post.objects.filter(
+                Q(title__icontains=q)
+                | Q(summary__icontains=q)
+                | Q(body__icontains=q),
+                status="Published")
+        elif '4P' in q:
+            queryset = Post.objects.filter(
+                Q(title__icontains=q)
+                | Q(summary__icontains=q)
+                | Q(body__icontains=q),
+                status="Published", course='1')
+        else:
+            queryset = Post.objects.filter(
+                Q(title__icontains=q)
+                | Q(summary__icontains=q)
+                | Q(body__icontains=q),
+                status="Published", course='1')
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
