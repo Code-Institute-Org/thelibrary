d4890dced9938fc0d561a4885d3b4e6384538504
Anna Greaves <gilhespy.anna@gmail.com>
2021-05-30 15:11:12 +0200

    Redirect non admins away from manager pg.

--- manager/views.py
+++ manager/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from posts.models import Post, PostFlag
 
 
+@login_required
 def manager_view(request):
+    """
+    Render manager pg for library admins.
+    Redirects non admins to the home page.
+    """
+    if not request.user.userprofile.is_admin:
+        return redirect('home')
+    else:
+        flags = PostFlag.objects.all()
+        flagged_posts = Post.objects.filter(flag__in=flags)
 
-    flags = PostFlag.objects.all()
-    flagged_posts = Post.objects.filter(flag__in=flags)
-
-    context = {
-        'flagged_posts': flagged_posts,
-    }
-    return render(request, 'manager.html', context)
+        context = {
+            'flagged_posts': flagged_posts,
+        }
+        return render(request, 'manager.html', context)
