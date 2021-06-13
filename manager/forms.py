0c8666b7709b3d745810a5b5e289d9ad5b39b891
Anna Greaves <gilhespy.anna@gmail.com>
2021-06-13 10:18:57 +0200

    Style manage user pg.

--- manager/forms.py
+++ manager/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms.widgets import Widget
+from users.models import UserProfile
 from posts.models import PostCategory, PostTag
 from slack.models import SlackChannel
 
@@ -19,3 +21,10 @@ class CreatePostTag(forms.ModelForm):
     class Meta:
         model = PostTag
         fields = ['name']
+
+
+class ManageUserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ['is_admin', 'is_mod', 'is_staff']
