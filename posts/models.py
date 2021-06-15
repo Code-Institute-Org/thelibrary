d5f9a48c74ef924b312ebf8d0fd1e5d25b61b220
Anna Greaves <gilhespy.anna@gmail.com>
2021-06-15 22:06:49 +0200

    Add alphabetical ordering to post categories.

--- posts/models.py
+++ posts/models.py
@@ -29,6 +29,9 @@ class PostCategory(models.Model):
     """
     name = models.CharField(max_length=20)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
