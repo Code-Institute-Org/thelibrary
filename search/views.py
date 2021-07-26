88a2ed0913de4d7aee1315fc208150df96666dc5
Anna Greaves <gilhespy.anna@gmail.com>
2021-07-26 11:46:17 +0200

    Add weights to text search fields and filter for specified course.

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.postgres import search
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector, SearchQuery, SearchRank)
 from django.db.models import Q
 from django.views.generic import ListView
 
@@ -18,37 +19,64 @@ class SearchResultsView(ListView):
         Checks if keywords specifying the course is in search query.
         If True, strips these keywords out of the query and filters
         results for course specified.
+
+        Uses search vectors from Post model fields:
+        title, summary, body, category and tags.
+        Ranks them in this order with title being most important
+        and category/tags least important.
         """
 
         q = self.request.GET.get('q')
 
-        # Basic version of text search before adding "django.contrib.postgres"
-
-        # if not any(course in q.lower() for course in ['4p', '5p']):
-        #     q = q.lower().replace('4p', '').replace('5p', '')
-        #     queryset = Post.objects.filter(
-        #         Q(title__icontains=q)
-        #         | Q(summary__icontains=q)
-        #         | Q(body__icontains=q),
-        #         status="Published")
-        # else:
-        #     course = '1' if '4p' in q.lower() else '2'
-        #     q = q.lower().replace('4p', '').replace('5p', '')
-        #     queryset = Post.objects.filter(
-        #         Q(title__icontains=q)
-        #         | Q(summary__icontains=q)
-        #         | Q(body__icontains=q),
-        #         status="Published", course=course)
-
         # For this code, I followed this tutorial from
-        # https://testdriven.io/blog/django-search/ 
-        search_vector = SearchVector(
-            "title", "summary", "body", "category", "tags"
-        )
-        search_query = SearchQuery(q)
-        queryset = Post.objects.annotate(
-            search=search_vector, rank=SearchRank(search_vector, search_query)
-        ).filter(search=search_query).order_by("-rank")
+        # https://testdriven.io/blog/django-search/
+
+        if not any(course in q.lower() for course in ['4p', '5p']):
+            q = q.lower().replace('4p', '').replace('5p', '')
+            search_vector = SearchVector(
+                "title", weight="A"
+            ) + SearchVector(
+                "summary", weight="B"
+            ) + SearchVector(
+                "body", weight="C"
+            ) + SearchVector(
+                "category", weight="D"
+            ) + SearchVector(
+                "tags", weight="D"
+            )
+            search_query = SearchQuery(q)
+            queryset = Post.objects.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(
+                search=search_query,
+                status="Published"
+            ).order_by("-rank")
+
+        else:
+            course = '1' if '4p' in q.lower() else '2'
+            q = q.lower().replace('4p', '').replace('5p', '')
+
+            search_vector = SearchVector(
+                "title", weight="A"
+            ) + SearchVector(
+                "summary", weight="B"
+            ) + SearchVector(
+                "body", weight="C"
+            ) + SearchVector(
+                "category", weight="D"
+            ) + SearchVector(
+                "tags", weight="D"
+            )
+            search_query = SearchQuery(q)
+            queryset = Post.objects.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(
+                search=search_query,
+                status="Published",
+                course=course
+            ).order_by("-rank")
 
         return queryset
 
