c44c24534d1f43c1a0ba047cbf66ce90c7022858
Anna Greaves <gilhespy.anna@gmail.com>
2021-07-27 09:50:26 +0200

    Add sales banner and js to persist the dismissal of the banner.

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,29 @@ setTimeout(function () {
     hideLoader();
 }, 600);
 
+
+/**
+ * Function sets a local storage value to remember
+ * the user has dismissed the sales banner. This then prevents the
+ * banner from being shown again on other pages.
+ * @param {event} e 
+ */
+function rememberClosed(e) {
+    e.preventDefault();
+
+    localStorage.setItem("salesBannerDismissed", true);
+}
+
+
+// Checks if sales-banner element exists, 
+// if it does: applies rememberClosed click event listener
+if (document.getElementById('sales-banner')) {
+    let salesBanner = document.getElementById('sales-banner');
+
+    salesBanner.addEventListener('click', rememberClosed);
+}
+
+// Removes sales-banner it exists, and salesBannerDismissed value is in local storage.
+if (localStorage.getItem('salesBannerDismissed') && document.getElementById('sales-banner')) {
+    document.getElementById('sales-banner').remove();
+}
