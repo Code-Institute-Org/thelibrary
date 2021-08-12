caa502f92a14177bbcf63c146a6547556171314c
A Greaves <anna.j.greaves@gmail.com>
2021-08-12 12:12:15 +0000

    Add slack login code from hackathon app  (WIP)

--- src/custom_slack_provider/apps.py
+++ src/custom_slack_provider/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class SlackLoginConfig(AppConfig):
-    name = 'slack_login'
+    name = 'custom_slack_provider'
--- src/thelibrary/settings.py
+++ src/thelibrary/settings.py
@@ -68,7 +68,6 @@ INSTALLED_APPS = [
     'slack',
     'courses',
     'manager',
-    'slack_login',
 ]
 
 MIDDLEWARE = [
@@ -121,12 +120,23 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_EMAIL_VERIFICATION = None
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = False
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 lOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
+SLACK_ENABLED = os.environ.get("SLACK_ENABLED") == 'True'
+
+if SLACK_ENABLED:
+    SLACK_WORKSPACE = os.environ.get('SLACK_WORKSPACE')
+    SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+    INSTALLED_APPS += ['custom_slack_provider']
+    SOCIALACCOUNT_PROVIDERS = {
+        'custom_slack_provider': {
+            'SCOPE':['identity.basic', 'identity.email'],
+        }
+    }
 
 WSGI_APPLICATION = 'thelibrary.wsgi.application'
 
